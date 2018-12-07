725778ffc6e756ff553b8c771061a53aa2c77b5d
Hanavan Kuhn <hanavan@cougar.cs.ksu.edu>
2018-12-07 17:25:42 -0600

    Fixed all the errors

--- core_chessboard.c
+++ core_chessboard.c
@@ -1,34 +1,21 @@
 #include "core_chessboard.h"
 #include <stdlib.h>
-
-const piece_t black_pawn = chess_create_piece(PAWN, BLACK);
-const piece_t black_rook = chess_create_piece(ROOK, BLACK);
-const piece_t black_knight = chess_create_piece(KNIGHT, BLACK);
-const piece_t black_bishop = chess_create_piece(BISHOP, BLACK);
-const piece_t black_queen = chess_create_piece(QUEEN, BLACK);
-const piece_t black_king = chess_create_piece(KING, BLACK);
-const piece_t white_pawn = chess_create_piece(PAWN, WHITE);
-const piece_t white_rook = chess_create_piece(ROOK, WHITE);
-const piece_t white_knight = chess_create_piece(KNIGHT, WHITE);
-const piece_t white_bishop = chess_create_piece(BISHOP, WHITE);
-const piece_t white_queen = chess_create_piece(QUEEN, WHITE);
-const piece_t white_king = chess_create_piece(KING, WHITE);
-
-const piece_t[CHESSBOARD_SIZE][CHESSBOARD_SIZE] default_chessboard = {
-	{ black_rook, black_knight, black_bishop, black_queen, black_king, black_bishop, black_knight, black_rook },
-	{ black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn, black_pawn },
-	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
-	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
-	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
-	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
-	{ white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn, white_pawn },
-	{ white_rook, white_knight, white_bishop, white_queen, white_king, white_bishop, white_knight, white_rook }
-};
+#include <string.h>
 
 chessboard_t * chess_create_default_chessboard() {
 	chessboard_t * board = malloc(sizeof(chessboard_t));
 	board->turncolor = WHITE;
-	board->pieces = default_chessboard;
+	piece_t * pieces[CHESSBOARD_SIZE][CHESSBOARD_SIZE] = {
+	{ BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK },
+	{ BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN },
+	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
+	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
+	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
+	{ NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
+	{ WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN },
+	{ WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK }
+	};
+	memcpy(board->pieces, pieces, CHESSBOARD_SIZE * CHESSBOARD_SIZE);
 	return board;
 }
 
@@ -48,9 +35,9 @@ color_t chess_get_tile_color(int rank, int file) {
     return (rank + file) % 2 == 0 ? WHITE : BLACK;
 }
 
-piece_t chess_create_piece(type_t type, color_t color) {
-    piece_t piece;
-    piece.type = type;
-    piece.color = color;
+piece_t * chess_create_piece(type_t type, color_t color) {
+    piece_t * piece = malloc(sizeof(piece_t));
+    piece->type = type;
+    piece->color = color;
     return piece;
 }
--- core_chessboard.h
+++ core_chessboard.h
@@ -3,6 +3,19 @@
 
 #define CHESSBOARD_SIZE 8
 
+#define BLACK_PAWN chess_create_piece(PAWN, BLACK)
+#define BLACK_ROOK chess_create_piece(ROOK, BLACK)
+#define BLACK_KNIGHT chess_create_piece(KNIGHT, BLACK)
+#define BLACK_BISHOP chess_create_piece(BISHOP, BLACK)
+#define BLACK_QUEEN chess_create_piece(QUEEN, BLACK)
+#define BLACK_KING chess_create_piece(KING, BLACK)
+#define WHITE_PAWN chess_create_piece(PAWN, WHITE)
+#define WHITE_ROOK chess_create_piece(ROOK, WHITE)
+#define WHITE_KNIGHT chess_create_piece(KNIGHT, WHITE)
+#define WHITE_BISHOP chess_create_piece(BISHOP, WHITE)
+#define WHITE_QUEEN chess_create_piece(QUEEN, WHITE)
+#define WHITE_KING chess_create_piece(KING, WHITE)
+
 
 typedef enum {
     BLACK,
@@ -25,17 +38,17 @@ typedef struct piece_t {
 
 typedef struct {
     color_t turncolor;
-    piece_t pieces[CHESSBOARD_SIZE][CHESSBOARD_SIZE];
+    piece_t * pieces[CHESSBOARD_SIZE][CHESSBOARD_SIZE];
 } chessboard_t;
 
 chessboard_t * chess_create_default_chessboard();
 void chess_move_piece(chessboard_t *, int, int, int, int);
-piece_t chess_get_piece_at(chessboard_t *, int, int);
+piece_t * chess_get_piece_at(chessboard_t *, int, int);
 int chess_to_rank_index(int);
 int chess_to_file_index(int);
 char chess_get_piece_char(piece_t);
 color_t chess_get_tile_color(int, int);
-piece_t chess_create_piece(type_t, color_t);
+piece_t * chess_create_piece(type_t, color_t);
 int chess_piece_is_valid_move(piece_t, int, int, piece_t, int, int, int, chessboard_t *);
 int chess_piece_is_piece_blocked(piece_t, int, int, piece_t, int, int, chessboard_t *);
 
@@ -53,19 +66,6 @@ int queen_is_piece_blocked(piece_t, int, int, piece_t, int, int, chessboard_t *)
 int king_is_valid_move(piece_t, int, int, piece_t, int, int, int, chessboard_t *);
 int king_is_piece_blocked(piece_t, int, int, piece_t, int, int, chessboard_t *);
 
-const piece_t black_pawn;
-const piece_t black_rook;
-const piece_t black_knight;
-const piece_t black_bishop;
-const piece_t black_queen;
-const piece_t black_king;
-const piece_t white_pawn;
-const piece_t white_rook;
-const piece_t white_knight;
-const piece_t white_bishop;
-const piece_t white_queen;
-const piece_t white_king;
-
-const piece_t[CHESSBOARD_SIZE][CHESSBOARD_SIZE] default_chessboard;
+piece_t default_chessboard[CHESSBOARD_SIZE][CHESSBOARD_SIZE];
 
 #endif
