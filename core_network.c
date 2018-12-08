7b773e1f9f37ada9466ae8a394c6f8e436d43daf
Swisher <cswisher42@gmail.com>
2018-12-07 21:52:56 -0600

    Added to core_network functionality, client_main, and client_ui.

--- core_network.c
+++ core_network.c
@@ -7,18 +7,21 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
 
 int core_create_client_socket(char * addr, int port){
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
+    int one = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+printf("%d\n", one);
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     //address.sin_addr.s_addr = "129.130.10.39";
     address.sin_port = htons(port);
-    inet_pton(AF_INET, addr, &address);
-    connect(sockfd, (struct sockaddr *) &address, sizeof(address));
+    int two = inet_pton(AF_INET, addr, &address.sin_addr);
+printf("%d\n", two);
+    int three = connect(sockfd, (struct sockaddr *) &address, sizeof(address));
+printf("%s\n", strerror(errno));
 
     return sockfd;
 }
